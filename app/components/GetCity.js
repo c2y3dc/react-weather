1a3a8faada73d41387b734e8c017ce8b54bf02e3
Ervin Chow <ervinchow@gmail.com>
2016-03-30 16:23:53 -0700

    adds basic implementation of GetCityContainer and scaffold for GetCity

---
+++ app/components/GetCity.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+function GetCity(props) {
+	return (
+		<button type="button" className="btn btn-lg btn-success">Get Weather</button>
+	)
+};
+
+module.exports = GetCity;
--- app/containers/GetCityContainer.js
+++ app/containers/GetCityContainer.js
@@ -1,10 +1,37 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var GetCity = require('../components/GetCity');
 
 var GetCityContainer = React.createClass({
+  getInitialState() {
+    return {
+      city: '',
+    };
+  },
+
+  getDefaultProps() {
+  	return {
+  		direction: 'column'
+  	};
+  },
+
+  handleSubmitCity: () => {
+    console.log(this.state.city);
+  },
+
+  handleUpdateCity: (e)=> {
+  	this.setState({
+  		city: e.target.value
+  	})
+  },
+
   render: () => (
-  	
-  	<button type="button" className="btn btn-lg btn-success" />
+  	<GetCity
+  		onSubmitCity={this.handleSubmitCity}
+  		onUpdateCity={this.handleUpdateCity}
+  		city={this.state.city}
+  		direction={this.props.direction}
+  	/>
   ),
 });
 
