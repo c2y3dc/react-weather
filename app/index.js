51cf1c72ef4a44d326c78dad866116804a70dde6
Ervin Chow <ervinchow@gmail.com>
2016-03-29 22:58:34 -0700

    adds base files, dependencies and webpack configs

---
+++ app/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var routes = require('./config/routes');
+
+ReactDOM.render(
+	<div>Hello World!</div>, document.getElementById('app')
+);
