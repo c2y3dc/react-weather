2e7302a900c7c6907ded32f0a4757ee402823657
Ervin Chow <ervinchow@gmail.com>
2016-03-30 20:58:20 -0700

    adds UI and style for header using GetCityContainer component

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,19 +1,31 @@
 var React = require('react');
+var GetCityContainer = require('./GetCityContainer');
 
 var styles = {
   container: {
     width: '100%',
-    height: '92%'
-  }
-}
+    height: '92%',
+  },
+  header:{
+  	display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'rgba(252, 90, 44, 0.89)',
+    color: '#fff',
+    padding: 5,
+  },
+};
 
-var Main = function(props){
-	return(
-		<div style={styles.container}>
-			<h1>Main.js Header</h1>
-			{props.children}
-		</div>
-	)
-}
+var Main = function (props) {
+  return (
+  <div style={styles.container}>
+			  <div style={styles.header}>
+				  <p style={{margin: 0}}>Clever Title</p>
+				  <GetCityContainer direction="row" />
+			  </div>
+			  {props.children}
+		  </div>
+  );
+};
 
 module.exports = Main;
